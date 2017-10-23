ff723c605bf3390b5577846e6b214165fee44777
Thomas Naude <thomas.naude@gmail.com>
2017-10-23 19:03:21 +0200

    active records with sinatra

--- app.rb
+++ app.rb
@@ -4,5 +4,53 @@ require "sinatra/reloader"
 require "sinatra/activerecord"
 
 get "/" do
-  "Hello world!"
+  @restaurants = Restaurant.all
+  erb :home
+end
+
+post "/restaurants" do
+  @restaurant = Restaurant.new(name: params[:name], address: params[:address])
+  @restaurant.save
+  redirect to "/"
+end
+
+get "/restaurants/new" do
+  erb :new
+end
+
+get "/restaurants/:id/edit" do
+  @restaurant = Restaurant.find(params[:id])
+  erb :edit
+end
+
+get "/restaurants/:id" do
+  @restaurant = Restaurant.find(params[:id])
+  erb :show
+  # find restaurant and assign to variable
+  # display restaurant view
+end
+
+put "/restaurants/:id" do
+  {"_method"=>"PUT", "name"=>"La Tour d'Argent", "address"=>"15ter Quai de la Tournelle, 75005 Paris", "captures"=>[], "id"=>"5"}
+  @restaurant = Restaurant.find(params[:id])
+  @restaurant.name = params[:name]
+  @restaurant.address = params[:address]
+  @restaurant.save
+  redirect to "/restaurants/#{@restaurant.id}"
+end
+
+delete "/restaurants/:id" do
+  restaurant = Restaurant.find(params[:id])
+  restaurant.destroy
+  redirect to "/"
+end
+
+get "/bordeaux" do
+  @restaurants = Restaurant.where('address LIKE ?', "%Bordeaux%")
+  erb :bordeaux
+end
+
+get "/best" do
+  @restaurant = Restaurant.order(rating: :desc, name: :asc).first
+  redirect to "/restaurants/#{@restaurant.id}"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,29 @@
+Restaurant.destroy_all
+
 tour_d_argent = Restaurant.new({
   name: "La Tour d'Argent",
-  address: "15 Quai de la Tournelle, 75005 Paris"
+  address: "15 Quai de la Tournelle, 75005 Paris",
+  rating: 5
 })
-tour_d_argent.save
+tour_d_argent.save!
 
 chez_gladines = Restaurant.new({
   name: "Chez Gladines",
-  address: "30 Rue des cinq Diamants, 75013 Paris"
+  address: "30 Rue des cinq Diamants, 75013 Paris",
+  rating: 3
 })
-chez_gladines.save
+chez_gladines.save!
+
+pizza_nico = Restaurant.new(
+  name: "Pizza Nico",
+  address: "Bordeaux",
+  rating: 1
+)
+pizza_nico.save!
+
+fufu = Restaurant.new(
+  name: "Fufu",
+  address: "Bordeaux",
+  rating: 4
+)
+fufu.save!
